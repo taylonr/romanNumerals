a93b99d288e6f2d5a75bbb1ee969f26750f2b99c
Brandon McAllister <brandon@insomniacisland.com>
2016-02-20 15:47:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Library Skeleton
+
+The building blocks for this skeleton came from this article by Krasimir Tsonev: http://krasimirtsonev.com/blog/article/javascript-library-starter-using-webpack-es6.
+
+This talk by Kate Hudson greatly influenced how npm scripts are being used: https://youtu.be/0RYETb9YVrk.
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var path = require('path');
+
+var libraryName = 'library';
+
+var plugins = [], outputFile;
+
+var mode = require('yargs').argv.mode;
+if (mode === 'min') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
