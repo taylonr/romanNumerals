e0cf1bd8acef188a475b7674eba9b1ecf35ab71c
Brandon McAllister <brandon@insomniacisland.com>
2016-02-20 15:48:42 -0600

    Cleanup eslint
    Updated eslint rules
    Added test specific eslintrc that includes mocha env
    Fixed all eslint issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,51 @@
+/* eslint-env node */
+
 var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 
 var libraryName = 'Library';
 
-var plugins = [], outputFile;
+var plugins = [],
+    outputFile;
 
 var mode = require('yargs').argv.mode;
 if (mode === 'min') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
+    plugins.push(new UglifyJsPlugin({
+        minimize: true
+    }));
+    outputFile = libraryName + '.min.js';
 } else {
-  outputFile = libraryName + '.js';
+    outputFile = libraryName + '.js';
 }
 
 var config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/lib',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
-  },
-  plugins: plugins
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/lib',
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        preLoaders: [{
+            test: /\.js$/,
+            loader: 'eslint-loader',
+            exclude: /node_modules/
+        }],
+        loaders: [{
+            test: /(\.js)$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        root: path.resolve('./src'),
+        extensions: ['', '.js']
+    },
+    plugins: plugins
 };
 
 module.exports = config;
